83afa228d29d612fa7838564c434fb8495a63c4a
rickshilling <rick.shilling@gmail.com>
2025-03-02 15:47:09 -0500

    3 2 2025

--- utils2.py
+++ utils2.py
@@ -5,20 +5,38 @@ from datetime import datetime
 from datetime import date
 
 def update_transactions(old_transactions:pd.DataFrame,
-                        new_transactions:pd.DataFrame,
+                        add_transactions:pd.DataFrame,
                         eps = 1e-6):
-    old_transactions = old_transactions.sort_values(by='TransactionDate', ascending=False)
-    new_transactions = new_transactions.sort_values(by='TransactionDate', ascending=False)
-    new_transactions = old_transactions
-    for current_transaction_row_index, current_transaction_row in current_transactions.iterrows():
-        found = False
-        for old_transaction_row_index, old_transaction_row in old_transactions.iterrows():
-            if equal_row(current_transaction_row, old_transaction_row):
-                found = True
-                break
-        if not found:
-            new_transactions = pd.concat([new_transactions, current_transaction_row], ignore_index=True)
-    pass
+    new_transactions = old_transactions.sort_values(by='TransactionDate')
+    add_transactions = add_transactions.sort_values(by='TransactionDate')
+    num_new_rows = len(new_transactions)
+    num_add_rows = len(add_transactions)
+    add_row_index = 0
+    add_row = add_transactions.iloc[add_row_index]
+    add_date = add_row['TransactionDate']
+    start_new_row_index = new_transactions['TransactionDate'].searchsorted(add_date)
+    while add_row_index < num_add_rows:
+        add_row = add_transactions.iloc[add_row_index]
+        # Iterate starting at new_transactions[new_row_index] until either
+        # new_row_index == num_new_rows or
+        # new_transactions[new_row_index] == add_transactions.iloc[add_row_index] 
+        stop = False
+        at_end = False
+        new_row_index = start_new_row_index
+        while not stop:
+            at_end = new_row_index == num_new_rows
+            if not at_end:
+                if equal_row(new_transactions.iloc[new_row_index], add_row):
+                    stop = True
+                else:
+                    new_row_index = new_row_index + 1
+            else:
+                stop = True
+        if at_end:
+            new_transactions = pd.concat([new_transactions, add_row.to_frame().T], ignore_index=True)
+        add_row_index = add_row_index + 1
+    new_transactions = new_transactions.sort_values(by='TransactionDate',ascending=False)
+    return new_transactions
 
 def equal_row(row1, row2, eps = 1e-6):
     result = \
